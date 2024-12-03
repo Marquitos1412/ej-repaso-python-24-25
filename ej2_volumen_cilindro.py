ced51e8aaf2980d136aae34791614d3c55487728
Marquitos1412 <marcubbon@alu.edu.gva.es>
2024-12-03 10:14:40 +0100

    ejercicio 2

--- ej2_volumen_cilindro.py
+++ ej2_volumen_cilindro.py
@@ -1,3 +1,7 @@
+#Importamos el numero pi
+
+from math import pi
+
 #Recibe los valores del radio y la altura y los convierte en float.
 
 radio = float(input("Introduzca el valor del radio del cilindro: "))
@@ -5,8 +9,9 @@ altura = float(input("Introduzca el valor de la altura del cilindro: "))
 
 #Calcula el volumen del cilindro y la redondea a dos digitos.
 
-volumen = round(radio * altura, 2)
+volumen = pi * radio**2 * altura
+volumen_redondeado = round(volumen, 2)
 
 #Escribe la respuesta.
 
-print(f"El volumen del cilindro es de {volumen}m cubicos")
+print(f"El volumen del cilindro es de {volumen_redondeado} cubicos")
