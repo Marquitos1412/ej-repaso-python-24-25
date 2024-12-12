edf1a534b4e1655378bf9a9ffe9d57d23a3a9b9b
Marquitos1412 <marcubbon@alu.edu.gva.es>
2024-12-12 13:38:19 +0100

    ej7 finalizado

--- ej_7_valor_mayor.py
+++ ej_7_valor_mayor.py
@@ -1,5 +1,20 @@
 lista_valores = []
 
-while len(lista_valores) < 2:
-    #Introducir valores
-    print("Hola")
+i = 0
+
+while i < 5:
+    print(f"Has introducido {i} numeros.")
+    try:
+        num_anadido = int(input(f"Introduce el siguiente numero: "))
+        lista_valores.append(num_anadido)
+        i += 1
+    except:
+        print("No es un numero.")
+
+valor_mayor = 0
+
+for valor in lista_valores:
+    if valor > valor_mayor:
+        valor_mayor = valor
+
+print(f"El numero mas grande de los que has introducido {lista_valores} es {valor_mayor}.")
